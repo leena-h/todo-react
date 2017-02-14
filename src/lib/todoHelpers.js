045ab7c31bb53eb2cc8cd0412f4c2403a2f03ba0
Leena Ha <leena@hhd.com.au>
2017-02-14 11:13:30 +1100

    Update todoHelpers.test.js

--- src/lib/todoHelpers.js
+++ src/lib/todoHelpers.js
@@ -1,3 +1,16 @@
 export const addTodo = (list, item) => [...list, item]
 
 export const generateId = () => Math.floor(Math.random()*1000000)
+
+export const findById = (id, list) => list.find(item => item.id === id)
+
+export const toggleTodo = (todo) => ({...todo, isComplete: !todo.isComplete})
+
+export const updateTodo = (list, updatedItem) => {
+  const updatedIndex = list.findIndex(item => item.id === updatedItem.id)
+  return [
+    ...list.slice(0, updatedIndex),
+    updatedItem,
+    ...list.slice(updatedIndex+1)
+  ]
+}
